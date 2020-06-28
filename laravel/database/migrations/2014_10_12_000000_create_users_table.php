388173fd5a27317fe3ea0d77bcb8bbbcef968f16
Kiet Tran <tuankiet1708@gmail.com>
2020-06-28 11:05:08 +0700

    is_admin

--- laravel/database/migrations/2014_10_12_000000_create_users_table.php
+++ laravel/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email', 50)->unique();
+            $table->boolean('is_admin')->default(false);
             $table->string('password');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();

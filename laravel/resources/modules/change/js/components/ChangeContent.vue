10f75cc8652aa9c98b347798a58cec0717c048ab
Kiet Tran <tuankiet1708@gmail.com>
2020-08-02 19:10:26 +0700

    xdebug

--- laravel/resources/modules/change/js/components/ChangeContent.vue
+++ laravel/resources/modules/change/js/components/ChangeContent.vue
@@ -2,14 +2,80 @@
     <v-card>
         <v-card-text>
             <form>
-                <v-text-field v-model="name" :error-messages="nameErrors" :counter="10" label="Name" required
-                    @input="$v.name.$touch()" @blur="$v.name.$touch()"></v-text-field>
-                <v-text-field v-model="email" :error-messages="emailErrors" label="E-mail" required
-                    @input="$v.email.$touch()" @blur="$v.email.$touch()"></v-text-field>
-                <v-select v-model="select" :items="items" :error-messages="selectErrors" label="Item" required
-                    @change="$v.select.$touch()" @blur="$v.select.$touch()"></v-select>
-                <v-checkbox v-model="checkbox" :error-messages="checkboxErrors" label="Do you agree?" required
-                    @change="$v.checkbox.$touch()" @blur="$v.checkbox.$touch()"></v-checkbox>
+                <v-row class="mb-n6">
+                    <v-col>
+                        <v-text-field
+                            v-model="id"
+                            label="Change ID"
+                            outlined
+                            disabled
+                        >
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row class="mb-n6">
+                    <v-col>
+                        <v-text-field
+                            v-model="title"
+                            :error-messages="titleErrors"
+                            :counter="255"
+                            label="Title"
+                            required
+                            @input="$v.title.$touch()"
+                            @blur="$v.title.$touch()"
+                            outlined>
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row class="mb-n6">
+                    <v-col>
+                        <v-textarea
+                            v-model="description"
+                            label="Description"
+                            @input="$v.description.$touch()"
+                            @blur="$v.description.$touch()"
+                            outlined>
+                        </v-textarea>
+                    </v-col>
+                </v-row>
+
+                <v-row class="mb-n6">
+                    <v-col>
+                        <v-text-field
+                            v-model="creator"
+                            label="Creator"
+                            @input="$v.creator.$touch()"
+                            @blur="$v.creator.$touch()"
+                            outlined
+                            disabled>
+                        </v-text-field>
+                    </v-col>
+                    <v-col>
+                        <v-text-field
+                            v-model="created_at"
+                            label="Created At"
+                            @input="$v.created_at.$touch()"
+                            @blur="$v.created_at.$touch()"
+                            outlined
+                            disabled>
+                        </v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="6">
+                        <v-text-field
+                            v-model="assigned_to"
+                            label="Assigned To"
+                            @input="$v.assigned_to.$touch()"
+                            @blur="$v.assigned_to.$touch()"
+                            outlined
+                            disabled>
+                        </v-text-field>
+                    </v-col>
+                </v-row>
 
                 <v-btn class="mr-4" @click="submit">submit</v-btn>
                 <v-btn @click="clear">clear</v-btn>
@@ -21,87 +87,46 @@
 <script>
     import {
         validationMixin
-    } from 'vuelidate'
+    } from 'vuelidate';
     import {
         required,
         maxLength,
         email
-    } from 'vuelidate/lib/validators'
+    } from 'vuelidate/lib/validators';
 
     export default {
         mixins: [validationMixin],
 
         validations: {
-            name: {
+            title: {
                 required,
-                maxLength: maxLength(10)
-            },
-            email: {
-                required,
-                email
-            },
-            select: {
-                required
-            },
-            checkbox: {
-                checked(val) {
-                    return val
-                },
-            },
+                maxLength: maxLength(255)
+            }
         },
 
         data: () => ({
-            name: '',
-            email: '',
-            select: null,
-            items: [
-                'Item 1',
-                'Item 2',
-                'Item 3',
-                'Item 4',
-            ],
-            checkbox: false,
+            id: null,
+            title: '',
+            description: null,
         }),
 
         computed: {
-            checkboxErrors() {
-                const errors = []
-                if (!this.$v.checkbox.$dirty) return errors
-                !this.$v.checkbox.checked && errors.push('You must agree to continue!')
-                return errors
-            },
-            selectErrors() {
-                const errors = []
-                if (!this.$v.select.$dirty) return errors
-                !this.$v.select.required && errors.push('Item is required')
-                return errors
-            },
-            nameErrors() {
-                const errors = []
-                if (!this.$v.name.$dirty) return errors
-                !this.$v.name.maxLength && errors.push('Name must be at most 10 characters long')
-                !this.$v.name.required && errors.push('Name is required.')
-                return errors
-            },
-            emailErrors() {
-                const errors = []
-                if (!this.$v.email.$dirty) return errors
-                !this.$v.email.email && errors.push('Must be valid e-mail')
-                !this.$v.email.required && errors.push('E-mail is required')
-                return errors
+            titleErrors() {
+                const errors = [];
+                if (!this.$v.title.$dirty) return errors;
+                !this.$v.title.required && errors.push('Title is required.');
+                return errors;
             },
         },
 
         methods: {
             submit() {
-                this.$v.$touch()
+                this.$v.$touch();
             },
             clear() {
-                this.$v.$reset()
-                this.name = ''
-                this.email = ''
-                this.select = null
-                this.checkbox = false
+                this.$v.$reset();
+                this.title = '';
+                this.description = '';
             },
         },
     }
